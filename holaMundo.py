a556bbf2c6ad331067de0c7edb8e70c2e96470a7
maikol159 <maikol2016.coronado@gmail.com>
2018-09-05 07:57:05 -0300

    hola ghtygyuyiuin

--- holaMundo.py
+++ holaMundo.py
@@ -1,6 +1,7 @@
 # Autor: Maikol Coronado
 # Fecha: 5 de septiembre de 2018
-# Codigo de pueba para utilizacion de git y  github
+# Codigo de prueba para utilizacion de git y  github
 
-print ("Hola mundo")
-print ("Chao mundo")
+print ("ashuuuuu")
+print ("salud")
+print ("gracias")
